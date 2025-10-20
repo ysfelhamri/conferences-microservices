67d7ad8caa59bc939925f83a95447797b4364656
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-20 09:38:19 +0100

    Added a Keynote Micro-Service

---
+++ keynote-service/src/test/java/q/jv/keynoteservice/KeynoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package q.jv.keynoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeynoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
