8bb661b58642b31c3b0dbb79008695d6210bdd42
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-20 09:48:43 +0100

    Added a Conference Micro-Service

---
+++ conference-service/src/main/java/q/jv/conferenceservice/ConferenceServiceApplication.java
@@ -0,0 +1,13 @@
+package q.jv.conferenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceServiceApplication.class, args);
+    }
+
+}
